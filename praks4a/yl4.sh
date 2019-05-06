33c435da611b353ac603fadcd8b0581dfc2b594d
viljar vissor <viljar.vissor@khk.ee>
2019-05-06 06:59:02 -0400

    pakkimine2

--- praks4a/yl4.sh
+++ praks4a/yl4.sh
@@ -2,8 +2,8 @@
 #kataloogi pakkimine
 echo -n "Millisest kaustast soovite backupi: "
 read kaust
-tar -czf test.tar.gz $kaust #teeb kasutaja soovitud kataloogi pakkitud kataloogiks
-mv test.tar.gz /home/user/skriptimine/praks4a/backup #liigutab kokku pakitud kataloogi backup kataloogi
+tar -czf $kaust.tar.gz $kaust #teeb kasutaja soovitud kataloogi pakkitud kataloogiks
+mv $kaust.tar.gz /home/user/skriptimine/praks4a/backup #liigutab kokku pakitud kataloogi backup kataloogi
 echo "Kataloogi $kaust backup-i nimi on $kaust.tar.gz ja ta asub /home/user/skriptimine/prask4a/backup kataloogis"
 #
 #Lõpp
