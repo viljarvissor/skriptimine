cbc4cbf96e75e55f078c14e1eb9e8ac30e9263fa
viljar vissor <viljar.vissor@khk.ee>
2019-05-06 06:50:24 -0400

    pakkimine1

--- praks4a/yl3.sh
+++ praks4a/yl3.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-#pakkimine
-echo -n "Millisest kaustast soovite backupi: "; read kaust
-asukoht="/home/user/skriptimine/praks4a/backup/"
-fail=$(echo $kaust | awk -F/ '{print $NF}')
-date=($date +"%Y-%m-%d")
-host=$(hostname -s)
-
-tar -czf $fail.tar.gz $fail
-mv $fail.tar.gz $asukoht/$date-$host-$fail.tar.gz
-
-echo "Fail $fail on pakitud asukohta $asukoht nime all $date-$host-$fail.tar.gz"
-#Lõpp
+#backup skript
+tar -czf /home/user/skriptimine/praks4a/backup/test.tar.gz /home/user/skriptimine/praks4a/test
+echo "kataloogi /home/user/skriptimine/praks4a/test/ backup-i nimi on test.tar.gz ja ta asub /home/user/skriptimine/praks4a/backup/ kataloogis"
+#
+#lõpp
