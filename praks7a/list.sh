2ab9d505afdb46678b7427468c00b9312b9ad055
viljar vissor <viljar.vissor@khk.ee>
2019-05-08 06:23:39 -0400

    Teeb kataloogi sisust listi ja loeb kokku faili tüüpide järgi

--- praks7a/list.sh
+++ praks7a/list.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-echo -e "Sisestage kataloogi nimi:  \c"
-read DIR
-ls -1
-NUMFILES=$(ls -l $DIR | tail +2 | wc -l)
-NUMORD=$(ls -l $DIR | grep -c ^-)
-NUMDIR=$(ls -l $DIR | grep -c ^d)
-NUMLINKS=$(ls -l $DIR | grep -c ^l)
-echo -e "Sul on selles kataloogis $NUMORD faili, $NUMDIR kataloogi ja $NUMLINKS linki"
+#
+#tuvastaja
+#
+echo -n "Sisesta kataloogi nimi:"
+read kataloog
+cd $kataloog
+find * -type f -exec echo fail: {} \;
+find * -type d -exec echo kataloog: {} \;
+find * -type l -exec echo link: {} \;
+NUMFILES=$(ls -l $kataloog | tail | wc -l)
+NUMORD=$(ls -l $kataloog | grep -c ^-)
+NUMDIR=$(ls -l $kataloog | grep -c ^d)
+NUMLINKS=$(ls -l $kataloog | grep -c ^l)
+echo "Selles kataloogis on $NUMORD faili, $NUMDIR kataloogi ja $NUMLINKS linki."
+#
+#skripti lõpp
