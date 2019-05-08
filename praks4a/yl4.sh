246893e7a0f098fdb2928afd0ee0d48a0e82b7cf
viljar vissor <viljar.vissor@khk.ee>
2019-05-08 05:51:07 -0400

    Backupi tegemine

--- praks4a/yl4.sh
+++ praks4a/yl4.sh
@@ -2,8 +2,12 @@
 #kataloogi pakkimine
 echo -n "Millisest kaustast soovite backupi: "
 read kaust
-tar -czf $kaust.tar.gz $kaust #teeb kasutaja soovitud kataloogi pakkitud kataloogiks
-mv $kaust.tar.gz /home/user/skriptimine/praks4a/backup #liigutab kokku pakitud kataloogi backup kataloogi
-echo "Kataloogi $kaust backup-i nimi on $kaust.tar.gz ja ta asub /home/user/skriptimine/prask4a/backup kataloogis"
+
+date=$(date +"$d.$m.$Y")
+aeg=$(date +"$R")
+baasnimi="$(basename $kaust)" #võtab ainult baasnime
+asukoht="/home/user/skriptimine/praks4a/backup/$baasnimi.backup.$aeg.$date.tar.gz"
+tar -czf $asukoht $kaust #teeb kasutaja soovitud kataloogi pakkitud kataloogiks
+echo "Kataloogi $kaust backup-i nimi on $baasnimi.backup.$aeg.$date.tar.gz ja ta asub /home/user/skriptimine/prask4a/backup kataloogis"
 #
 #Lõpp
